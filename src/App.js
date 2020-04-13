b8a51afb53a3a6b07ec2ecf4ea1c40653b347751
rodrigo Hettwer <rodrigo.hettwer@gmail.com>
2020-04-13 00:31:58 -0300

    Edited App.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,63 @@
-import React from "react";
+import React, { useEffect, useState} from "react";
+import api from './services/api';
 
 import "./styles.css";
 
-function App() {
+export default function App() {
+
+  const [repositories, setRepositories] = useState([]);
+  const [title, setTitle] = useState('');
+  const [url, setUrl] = useState('');
+  const [techs, setTechs] = useState('');
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    });
+  },[]);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title,
+      url,
+      techs
+    });
+    const repository = response.data;
+    setRepositories([...repositories, repository]);
+    setTitle('');
+    setUrl('');
+    setTechs('');
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+    const newRepositories = [...repositories];
+    const repositoryIndex = repositories.findIndex(repository =>repository.id === id);
+    newRepositories.splice(repositoryIndex, 1);
+    setRepositories(newRepositories);
   }
 
   return (
     <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
-      </ul>
-
-      <button onClick={handleAddRepository}>Adicionar</button>
+        <label>Title</label>
+          <input onChange={(e) => setTitle(e.target.value)} value={title} type="text" />
+        <label>Url</label>
+          <input onChange={(e) => setUrl(e.target.value)} value={url} type="text" />
+        <label>Techs</label>
+          <input onChange={(e) => setTechs(e.target.value)} value={techs} type="text" />
+        <button onClick={handleAddRepository}>Adicionar</button>
+        <ul data-testid="repository-list">
+          {repositories.map((repository) => (
+          <li key={repository.id}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+          ))}
+        </ul>
     </div>
   );
 }
 
-export default App;
+
